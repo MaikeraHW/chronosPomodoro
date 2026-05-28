b7fb5f1fc72b71b9fd184cb15b3e90965708e802
MaikeraW <wagner.maiconhenrique@gmail.com>
2026-05-28 16:26:18 -0300

    settado o botão para mudar o tema do app

--- src/components/buttons/button/Button.tsx
+++ src/components/buttons/button/Button.tsx
@@ -3,12 +3,14 @@ import styles from './Button.module.css'
 
 type ButtonProps ={
     icon: LucideIcon
+    title: string
+    onClick?: () => void
 }
 
-function Button({icon: Icon }:ButtonProps){
+function Button({icon: Icon, title, onClick }:ButtonProps){
 
     return (
-        <a className={styles.btn}><Icon size={32}/></a>
+        <a className={styles.btn} title={title} onClick={onClick}><Icon size={32}/></a>
     )
 }
 
--- src/components/buttons/buttonsHolder/ButtonsHolder.tsx
+++ src/components/buttons/buttonsHolder/ButtonsHolder.tsx
@@ -1,15 +1,31 @@
 import { HomeIcon, History, Settings, Sun } from 'lucide-react'
 import Button from '../button/Button'
 import styles from './ButtonsHolder.module.css'
+import { useEffect, useState } from 'react'
+
+type AvailableThemes = 'dark' | 'light'
+
 
 function ButtonsHolder(){
 
+    const [theme, setTheme] = useState<AvailableThemes>('dark')
+
+    function handleChangeTheme(){
+
+        theme === 'dark' ? setTheme('light') : setTheme('dark')
+    }
+
+    useEffect( () => {
+        document.documentElement.setAttribute('data-theme', theme)
+    }, [theme])
+
+
     return (
         <div className={styles.btnHolder}>
-            <Button icon={HomeIcon}/>
-            <Button icon={History}/>
-            <Button icon={Settings}/>
-            <Button icon={Sun}/>
+            <Button icon={HomeIcon} title="Ir para a Home"/>
+            <Button icon={History} title="Ver histórico"/>
+            <Button icon={Settings} title="Configurações"/>
+            <Button icon={Sun} title="Mudar Tema" onClick={handleChangeTheme}/>
         </div>
     )
 }
